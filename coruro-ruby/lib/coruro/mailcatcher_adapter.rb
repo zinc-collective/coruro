aea23a34229d7a22ec8c82f1953e70ea460a50e9
Zee Spencer <zee@wecohere.com>
2018-02-14 17:57:50 -0800

    Listen on all ips

--- coruro-ruby/lib/coruro/mailcatcher_adapter.rb
+++ coruro-ruby/lib/coruro/mailcatcher_adapter.rb
@@ -85,7 +85,7 @@ class Coruro
       def start(config)
         return if up?(config)
         self.stdin, self.stdout, self.stderr, self.thread =
-          Open3.popen3({ "PATH" => ENV['PATH'] }, 'mailcatcher -f', { unsetenv_others:true })
+          Open3.popen3({ "PATH" => ENV['PATH'] }, 'mailcatcher -f --ip=0.0.0.0', { unsetenv_others:true })
 
         p "Status", self.thread.status
       end
